c4ebd9e904484a20f6ad2b3fe524ca99cfd6855b
showrav dhar <dhardip349900@gmail.com>
2024-07-09 14:05:59 +0600

    until 10.59

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,2 +1,35 @@
 import 'package:flutter/material.dart';
 
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffecefe8),
+      body: Container(
+          margin: EdgeInsets.only(top: 50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset("images/EDU_MEDIX.jpg"),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "Daily Meidcal\nNeeds at your\nDoorstep",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40.00,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
